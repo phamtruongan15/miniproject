3dafb9a68a0b7216e029ab3ed1b3e23e58360925
AnPNT <aannguyen1405@gmail.com>
2024-01-26 11:00:44 +0700

    commit(26/1)

--- Order/urls.py
+++ Order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import OrderGetView, OrderPostView, OrderUpdateView, OrderDeleteView
 
 urlpatterns = [
-    path('Order/<int:pk>/get/', OrderGetView.as_view(), name='modelb-list'),
+    path('Order/<int:pk>/', OrderGetView.as_view(), name='modelb-list'),
     path('post/', OrderPostView.as_view(), name='modelb-list'),
     path('update/<int:pk>/', OrderUpdateView.as_view(), name='modelb-list'),
     path('delete/<int:pk>/', OrderDeleteView.as_view(), name='modelb-list'),
