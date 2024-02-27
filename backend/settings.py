fbb4106529716552a7c517e2e623e04306496139
AnPNT <aannguyen1405@gmail.com>
2024-02-27 16:31:37 +0700

    commit(27/2/2024)

--- backend/settings.py
+++ backend/settings.py
@@ -99,10 +99,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_db',
+        'USER': 'root',
+        'PASSWORD': 'lp123456',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3307',      # or the port on which your MySQL server is listening
+    }
 }
 
 
